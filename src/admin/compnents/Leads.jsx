b9965297a37f98d75c58f96745df9c3b6f86ac6b
Mern-Tapas <cyb6261452510@gmail.com>
2023-02-09 23:22:43 +0530

    udpate

--- src/admin/compnents/Leads.jsx
+++ src/admin/compnents/Leads.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
+import "../../assets/css/leads.css"
+import LeadsItems from '../compnents/LeadsItems'
 
 function Leads() {
   return (
-    <>
-    
-    </>
+    <div className='leads-pages ' style={{ height: "100%" }}>
+      <div >
+
+      </div>
+      <div style={{overflow:"scroll"}}>
+        <LeadsItems/>
+      </div>
+      <div >
+        <Outlet/>
+      </div>
+    </div>
   )
 }
 

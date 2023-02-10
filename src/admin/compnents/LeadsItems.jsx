476ea1264d128ca08af69597434e0d6acf6a7a6a
reactpd <cyb.8964803609@gmail.com>
2023-02-10 16:40:42 +0530

    update

--- src/admin/compnents/LeadsItems.jsx
+++ src/admin/compnents/LeadsItems.jsx
@@ -6,27 +6,29 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { Button } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 function LeadsItems() {
 
 
     const rows = [
-        {name:"username", calories:"6261452510", fat:"Bank Loan", carbs:"5", protein:"5"},
-        {name:"username", calories:"6261452510", fat:"Bank Loan", carbs:"5", protein:"5"},
-        {name:"username", calories:"6261452510", fat:"Bank Loan", carbs:"5", protein:"5"},
-        {name:"username", calories:"6261452510", fat:"Bank Loan", carbs:"5", protein:"5"},
-        {name:"username", calories:"6261452510", fat:"Bank Loan", carbs:"5", protein:"5"},
-        {name:"username", calories:"6261452510", fat:"Bank Loan", carbs:"5", protein:"5"},
-        {name:"username", calories:"6261452510", fat:"Bank Loan", carbs:"5", protein:"5"},
-        {name:"username", calories:"6261452510", fat:"Bank Loan", carbs:"5", protein:"5"},
-        {name:"username", calories:"6261452510", fat:"Bank Loan", carbs:"5", protein:"5"},
-        {name:"username", calories:"6261452510", fat:"Bank Loan", carbs:"5", protein:"5"},
-        {name:"username", calories:"6261452510", fat:"Bank Loan", carbs:"5", protein:"5"},
-        {name:"username", calories:"6261452510", fat:"Bank Loan", carbs:"5", protein:"5"},
-        {name:"username", calories:"6261452510", fat:"Bank Loan", carbs:"5", protein:"5"},
-        {name:"username", calories:"6261452510", fat:"Bank Loan", carbs:"5", protein:"5"},
-        {name:"username", calories:"6261452510", fat:"Bank Loan", carbs:"5", protein:"5"},
-        {name:"username", calories:"6261452510", fat:"Bank Loan", carbs:"5", protein:"5"},
+        { name: "username", calories: "6261452510", fat: "Bank Loan", carbs: "5", protein: "5" },
+        { name: "username", calories: "6261452510", fat: "Bank Loan", carbs: "5", protein: "5" },
+        { name: "username", calories: "6261452510", fat: "Bank Loan", carbs: "5", protein: "5" },
+        { name: "username", calories: "6261452510", fat: "Bank Loan", carbs: "5", protein: "5" },
+        { name: "username", calories: "6261452510", fat: "Bank Loan", carbs: "5", protein: "5" },
+        { name: "username", calories: "6261452510", fat: "Bank Loan", carbs: "5", protein: "5" },
+        { name: "username", calories: "6261452510", fat: "Bank Loan", carbs: "5", protein: "5" },
+        { name: "username", calories: "6261452510", fat: "Bank Loan", carbs: "5", protein: "5" },
+        { name: "username", calories: "6261452510", fat: "Bank Loan", carbs: "5", protein: "5" },
+        { name: "username", calories: "6261452510", fat: "Bank Loan", carbs: "5", protein: "5" },
+        { name: "username", calories: "6261452510", fat: "Bank Loan", carbs: "5", protein: "5" },
+        { name: "username", calories: "6261452510", fat: "Bank Loan", carbs: "5", protein: "5" },
+        { name: "username", calories: "6261452510", fat: "Bank Loan", carbs: "5", protein: "5" },
+        { name: "username", calories: "6261452510", fat: "Bank Loan", carbs: "5", protein: "5" },
+        { name: "username", calories: "6261452510", fat: "Bank Loan", carbs: "5", protein: "5" },
+        { name: "username", calories: "6261452510", fat: "Bank Loan", carbs: "5", protein: "5" },
     ]
 
 
@@ -37,7 +39,7 @@ function LeadsItems() {
                 <Table sx={{ minWidth: 650 }} aria-label="simple table">
                     <TableHead>
                         <TableRow>
-                            <TableCell>Product</TableCell>
+                            <TableCell>Loan</TableCell>
                             <TableCell align="right">Name</TableCell>
                             <TableCell align="right">Contact</TableCell>
                             <TableCell align="right">Salary</TableCell>
@@ -57,8 +59,12 @@ function LeadsItems() {
                                 <TableCell align="right">{row.calories}</TableCell>
                                 <TableCell align="right">{row.fat}</TableCell>
                                 <TableCell align="right">{row.carbs}</TableCell>
-                                <TableCell align="right">{row.protein}</TableCell>
-                                <TableCell align="right">{row.protein}</TableCell>
+                                <TableCell align="right" className='d-flex'> <p className='ms-auto my-auto p-1 px-2 rounded-pill button-red'>verified</p> </TableCell>
+                                <TableCell align="right">
+                                    <Link to="656468656">
+                                        <Button className='ms-2' variant="contained" size="small">view</Button>
+                                    </Link>
+                                </TableCell>
                             </TableRow>
                         ))}
                     </TableBody>

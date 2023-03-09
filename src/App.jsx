9c20e3e427b5a2cd2418100da2b999749f6fdee9
reactpd <cyb.8964803609@gmail.com>
2023-03-09 10:57:32 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Login from "./client/pages/Login"
 import Signup from "./client/pages/Signup"
 import Dashboard from './admin/pages/Dashboard'
 import Create from './admin/pages/Create'
-import Leads from './admin/pages/Leads'
 import Analytics from './admin/pages/Analytics'
 import Files from './admin/pages/Files'
 import Users from './admin/pages/Users'
@@ -24,9 +23,7 @@ function App() {
         <Route path='Dashboard' element={<Dashboard />} >
           <Route path='' element={<Analytics />} />
           <Route path='create' element={<Create />} />
-          <Route path='leads' element={<Leads />} >
-
-          </Route>
+         
           <Route path='files' element={<Files />} />
           <Route path='users' element={<Users />} />
           <Route path='setting' element={<Setting />} />
